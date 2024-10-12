2174e0946697ec9d5a67e331fd683c464daacab6
JWen33 <jww1033@gmail.com>
2024-10-13 01:01:22 +0800

    style 修改數據頁面樣式與RWD
    修改樣式符合bunnySugar，並加上RWD
    
    2024/10/13 01:01

--- src/views/dashboard/SalesStatistics.vue
+++ src/views/dashboard/SalesStatistics.vue
@@ -1,42 +1,46 @@
 <template>
   <DashBoardNavBarOrder />
 
-  <div class="container-fluid">
+  <div class="container-fluid salesData">
+    <h1 class="my-3 salesTitle">銷售數據表</h1>
+    
     <div class="row">
-      <h1 class="my-3">銷售數據表</h1>
-      
-      <div class="col-md-6">
-        <h2 class="mt-4">各產品營收比重</h2>
-        <PieChart />
+      <div class="col-md-6 mb-4">
+        <div class="dataContainer revenueChart">
+          <h2 class="mt-4">各產品營收比重</h2>
+          <PieChart />
+        </div>
       </div>
       
-      <div class="col-md-6">
-        <h2 class="mt-4">商品銷售詳情</h2>
-        <div class="contentContainer">
-          <table class="contentTable">
-            <thead>
-              <tr class="content">
-                <th>商品編號</th>
-                <th>商品名稱</th>
-                <th class="text-end">售出數量</th>
-                <th class="text-end">營收</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(item, index) in salesData" :key="index">
-                <td>{{ item.productId }}</td>
-                <td>{{ item.productName }}</td>
-                <td class="text-end">{{ item.sumQuantity }}</td>
-                <td class="text-end">{{ item.sumPrice }}</td>
-              </tr>
-            </tbody>
-            <tfoot>
-              <tr>
-                <td colspan="3" class="text-end">總營業額</td>
-                <td class="text-end">{{ totalRevenue }}</td>
-              </tr>
-            </tfoot>
-          </table>
+      <div class="col-md-6 mb-4">
+        <div class="dataContainer salesDetails">
+          <h2 class="mt-4">商品銷售詳情</h2>
+          <div class="contentContainer">
+            <table class="contentTable">
+              <thead>
+                <tr class="content">
+                  <th>商品編號</th>
+                  <th>商品名稱</th>
+                  <th>售出數量</th>
+                  <th>營收</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(item, index) in salesData" :key="index">
+                  <td>{{ item.productId }}</td>
+                  <td>{{ item.productName }}</td>
+                  <td class="dataInfo">{{ item.sumQuantity }}</td>
+                  <td class="dataInfo">{{ item.sumPrice }}</td>
+                </tr>
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colspan="3" class="dataInfo">總營業額</td>
+                  <td class="dataInfo">{{ totalRevenue }}</td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
         </div>
       </div>
     </div>
@@ -78,11 +82,38 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.contentContainer {
-  width: 100%;
-  padding: 1% 2%;
+.salesData {
+  color: rgba(50, 67, 95, 1);
+}
+
+.dataContainer {
   background-color: white;
   box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  padding: 20px;
+  border-radius: 5px;
+  height: 100%;
+}
+
+.revenueChart h2,
+.salesDetails h2 {
+  color: rgba(143, 134, 129, 1);
+  margin-bottom: 20px;
+}
+
+.contentContainer {
+  width: 100%;
+  background-color: white;
+}
+
+.salesTitle {
+    display: flex;
+    width: 100%;
+    height: 2%;
+    color: rgb(74, 95, 128);
+    font-size: 2.2vw;
+    font-weight: bold;
+    margin-bottom: 3%;
+    cursor: pointer;
 }
 
 .contentTable {
@@ -120,5 +151,29 @@ tr:first-child th:last-child {
 
 tr:last-child td:last-child {
   border-bottom-right-radius: 5px;
+}
+
+/* 大於768px */
+@media (min-width: 769px) {
+  .row {
+    display: flex;
+  }
+  
+  .col-md-6 {
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
+}
+
+/* 小於768px */
+@media (max-width: 768px) {
+  .col-md-6 {
+    width: 100%;
+  }
+  
+  .revenueChart,
+  .salesDetails {
+    margin-bottom: 20px;
+  }
 }
 </style>
