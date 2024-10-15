1904b64b81982c9a5b8c1812d1737fc759a67ac4
JWen33 <jww1033@gmail.com>
2024-10-16 00:27:41 +0800

    style 修改數據圖表顏色
    相近顏色做點區隔
    
    2024/10/16 12:27

--- src/views/dashboard/SalesStatistics.vue
+++ src/views/dashboard/SalesStatistics.vue
@@ -82,8 +82,8 @@ const chartData = computed(() => {
     datasets: [{
       data: salesData.value.map(item => item.totalSales),
       backgroundColor: [
-          '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF',
-          '#FF9F40', '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0'
+        '#FF7A94', '#5CB8F0', '#FFD76E', '#65CCCC', '#A37BFF',
+        '#FFB055', '#FF4F74', '#2592DF', '#FFBE40', '#3BAEA6'
         ]
     }]
   };
